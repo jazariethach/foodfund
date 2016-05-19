050b1bbfa167d2f55e04ca2aff76fdeb76b625ab
Felicia <xftruong@hotmail.com>
2016-05-18 21:38:04 -0700

    Basic form

--- public/test.jsx
+++ public/test.jsx
@@ -1,9 +1,51 @@
 var Jaz = React.createClass({
     	render: function() {
     		return(
-    			<div className='jaz'>
-    				<h1> Hi Jaz, I am in a class</h1>
-    			</div>
+        <form>
+          <div className="form-group row">
+            <label for="inputEmail3" className="col-sm-2 form-control-label">Event Title</label>
+            <div className="col-sm-6">
+              <input className="form-control" placeholder="Event Title"></input>
+            </div>
+          </div>
+
+          <div className="form-group row">
+            <label for="inputEmail3" className="col-sm-2 form-control-label">Organization Name</label>
+            <div className="col-sm-6">
+              <input className="form-control" placeholder="Organization Name"></input>
+            </div>
+          </div>
+
+          <div className="form-group row">
+            <label for="inputEmail3" className="col-sm-2 form-control-label">Location</label>
+            <div className="col-sm-6">
+              <input className="form-control" placeholder="Location"></input>
+            </div>
+          </div>
+          <div className="form-group row">
+            <label for="inputEmail3" className="col-sm-2 form-control-label">Start Date</label>
+            <div className="col-sm-6">
+              <input className="form-control" placeholder="Start Date"></input>
+            </div>
+          </div>
+          <div className="form-group row">
+            <label for="inputEmail3" className="col-sm-2 form-control-label">End Date</label>
+            <div className="col-sm-6">
+              <input className="form-control" placeholder="End Date"></input>
+            </div>
+          </div>
+          <div className="form-group row">
+            <label for="inputEmail3" className="col-sm-2 form-control-label">Description</label>
+            <div className="col-sm-6">
+              <input className="form-control" placeholder="Description"></input>
+            </div>
+          </div>
+          <div className="form-group row">
+            <div className="col-sm-offset-2 col-sm-6">
+              <button type="submit" className="btn btn-secondary">Submit</button>
+            </div>
+          </div>
+        </form>
     		)
     	}
     })

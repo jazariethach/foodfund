33d26a8a5c5d1deabee7ae95616a3588c08aecba
Felicia <xftruong@hotmail.com>
2016-05-19 19:27:02 -0700

    Working modal

--- public/test.jsx
+++ public/test.jsx
@@ -18,64 +18,80 @@ var App = React.createClass({
         }.bind(this);
       },
       onSubmit: function(e) {
-        e.preventDefault()    
+        e.preventDefault()
         $.post("http://localhost:3000/create_event", this.state, function(data){
           if(data==='done') {
             alert('form submitted!')
           }
-        });           
+        });
       },
       render: function() {
         return(
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group row">
-            <label for="inputEmail3" className="col-sm-2 form-control-label">Event Title</label>
-            <div className="col-sm-6">
-              <input className="form-control" placeholder="Event Title" onChange={this.handleChange('title')} value={this.state.title}></input>
+        <div className="container">
+          <button type="button" className="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Create Fundraising Event</button>
+
+          <div className="modal fade" id="myModal" role="dialog">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <button type="button" className="close" data-dismiss="modal">&times;</button>
+                  <h4 className="modal-title">Create Fundraising Event</h4>
+                </div>
+                <div className="modal-body">
+                <form onSubmit={this.onSubmit}>
+
+                  <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-3 form-control-label">Event Title</label>
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Event Title" onChange={this.handleChange('title')} value={this.state.title}></input>
+                    </div>
+                  </div>
+
+                  <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-3 form-control-label">Organization</label>
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Organization Name" onChange={this.handleChange('name')} value={this.state.name}></input>
+                    </div>
+                  </div>
+
+                  <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-3 form-control-label">Location</label>
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Location" onChange={this.handleChange('location')} value={this.state.location}></input>
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-3 form-control-label">Start Date</label>
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Start Date" onChange={this.handleChange('start')} value={this.state.start}></input>
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-3 form-control-label" onChange={this.handleChange('end')} value={this.state.end}>End Date</label>
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="End Date" onChange={this.handleChange('end')} value={this.state.end}></input>
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-3 form-control-label">Description</label>
+                    <div className="col-sm-6">
+                      <input className="form-control" placeholder="Description" onChange={this.handleChange('desc')} value={this.state.desc}></input>
+                    </div>
+                  </div>
+                </form>
+                </div>
+                <div className="modal-footer">
+                  <button type="submit" className="btn btn-success" data-dismiss="modal">Submit</button>
+                </div>
+              </div>
+
             </div>
           </div>
 
-          <div className="form-group row">
-            <label for="inputEmail3" className="col-sm-2 form-control-label">Organization Name</label>
-            <div className="col-sm-6">
-              <input className="form-control" placeholder="Organization Name" onChange={this.handleChange('name')} value={this.state.name}></input>
-            </div>
-          </div>
-
-          <div className="form-group row">
-            <label for="inputEmail3" className="col-sm-2 form-control-label">Location</label>
-            <div className="col-sm-6">
-              <input className="form-control" placeholder="Location" onChange={this.handleChange('location')} value={this.state.location}></input>
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="inputEmail3" className="col-sm-2 form-control-label">Start Date</label>
-            <div className="col-sm-6">
-              <input className="form-control" placeholder="Start Date" onChange={this.handleChange('start')} value={this.state.start}></input>
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="inputEmail3" className="col-sm-2 form-control-label" onChange={this.handleChange('end')} value={this.state.end}>End Date</label>
-            <div className="col-sm-6">
-              <input className="form-control" placeholder="End Date" onChange={this.handleChange('end')} value={this.state.end}></input>
-            </div>
-          </div>
-          <div className="form-group row">
-            <label for="inputEmail3" className="col-sm-2 form-control-label">Description</label>
-            <div className="col-sm-6">
-              <input className="form-control" placeholder="Description" onChange={this.handleChange('desc')} value={this.state.desc}></input>
-            </div>
-          </div>
-          <div className="form-group row">
-            <div className="col-sm-offset-2 col-sm-6">
-              <button type="submit" className="btn btn-secondary">Submit</button>
-            </div>
-          </div>
-        </form>
+        </div>
         )
       }
     })
-
 
     ReactDOM.render(
       <App />,
